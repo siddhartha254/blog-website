3c7827636829d6a047cd2a654c2e42470849fb9d
Siddhartha <siddhartha.arora4@gmail.com>
2023-06-14 04:59:21 +0530

    fixed navigation bug

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css"
+import { Navigate } from "react-router-dom";
 
 const modules = {
     toolbar: [
@@ -26,6 +27,8 @@ export default function Create(){
     const [content, setContent] = useState('');
     const [files, setFiles] = useState('');
 
+    const [redirect, setRedirect] = useState(false);
+
     async function createNewPost(ev){
         
         const data = new FormData();
@@ -41,7 +44,14 @@ export default function Create(){
             method: 'POST',
             body: data,
         });
-        console.log(await response.json());
+        
+        if(response.ok){
+            setRedirect(true);
+        }
+    }
+
+    if(redirect){
+        return <Navigate to={'/'} />
     }
 
     return(
